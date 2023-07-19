fd60cb4a8091b41dfd11e7747b5df2bed8f04d33
ttwofacess <ttwofacess@gmail.com>
2023-07-18 21:43:03 -0400

    Definiendo y aplicando 2 fuentes.

--- descuento.php
+++ descuento.php
@@ -12,8 +12,9 @@ session_start();
     <title>Descuentos</title>
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
     <header><h1>Superstore</h1></header>
