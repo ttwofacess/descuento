f56209320ae4cef7e9c1022551ab71f8c683a562
ttwofacess <20799318+ttwofacess@users.noreply.github.com>
2024-02-12 06:50:47 -0500

    Quitar codigo reemplazado

--- descuento.php
+++ descuento.php
@@ -59,56 +59,6 @@ echo "
             <input class='inpPrc' id='dsct3' type='text' name='dsct3' value='".$_SESSION['dsct3']."' >
         </article>
 
-        <div class='divsGralCfg'>
-            
-            <!-- <div class='pics divsGralCfg'>
-                <label class='lblimg' id='lblimg1' for='img1'>Reverse T-shirt</label>
-                <img id='img1' src='img/rem-using.png' alt='t-shirt'><br />
-            </div> 
-            --> 
-
-            <!-- <div class='pics divsGralCfg'>
-                <label class='lblimg' id='lblimg2' for='img2'>Aesthetic Suglasses</label>
-                <img id='img2' src='img/sha-using.png' alt='shades'>
-            </div>
-            -->
-
-            <!-- <div class='pics divsGralCfg'>
-                <label class='lblimg' for='img3'>Sneakers</label>
-                <img id='img3' src='img/zap-using.png' alt='sneakers'>
-            </div>
-            -->
-        </div>
-
-
-        <div class='divsGralCfg nopics'>
-            <!-- <label class='lblPrc' id='ingPrecio_lblitem1' for='prc1' >Ingrese precio</label>
-            <input class='inpPrc' id='prc1' type='number' name='prc1' value='".$_SESSION['prc1']."'>
-            -->
-
-            <!-- <label class='lblPrc' id='ingPrecio_lblitem2' for='prc2'>Ingrese precio</label>
-            <input class='inpPrc' id='prc2' type='number' name='prc2' value='".$_SESSION['prc2']."'>
-            -->
-
-            <!--<label class='lblPrc' id='ingPrecio_lblitem3' for='prc3'>Ingrese precio</label>
-            <input class='inpPrc' id='prc3' type='number'  name='prc3' value='".$_SESSION['prc3']."'>
-            -->
-        </div>
-
-        <div class='divsGralCfg ingDsct'>
-            <!-- <label class='lblPrc' id='ingDsct_lblitem1' for='dsct1'>Descuento aplicado</label>    
-            <input class='inpPrc' id='dsct1' type='text' name='dsct1' value='".$_SESSION['dsct1']."'>
-            -->
-
-            <!-- <label class='lblPrc' id='ingDsct_lblitem2' for='dsct2'>Descuento aplicado</label>
-            <input class='inpPrc' id='dsct2' type='text' name='dsct2' value='".$_SESSION['dsct2']."' >
-            -->
-
-            <!--<label class='lblPrc' id='ingDsct_lblitem3' for='dsct3'>Descuento aplicado</label>
-            <input class='inpPrc' id='dsct3' type='text' name='dsct3' value='".$_SESSION['dsct3']."' >
-            -->
-        </div>
-
         <input type='submit' value='Consultar'><br />
         </form>
     </main>
